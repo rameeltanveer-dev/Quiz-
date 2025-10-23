ba72248d4568b6850ff0cf0eaefaabd536684161
Rameel Tanveer <rameeltanveer25@gmail.com>
2025-10-24 00:04:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -96,13 +96,7 @@ startWithName.addEventListener('click', ()=>{
   return; 
 }
 
-if (!canAttempt(val)) { 
-  // If this device has already attempted the quiz with this name — اگر اس نام سے اس ڈیوائس پر پہلے ہی کوئز حل کیا جا چکا ہے
-  loginMsg.textContent = "اس نام سے اس ڈیوائس پر پہلے ہی کوئز حل کیا جا چکا ہے / This name has already been used on this device"; 
-  return; 
-}
-  userName = val; markAttempt(userName); beginQuiz();
-});
+
 
 /* ---------- BEGIN QUIZ ---------- */
 function beginQuiz(){
